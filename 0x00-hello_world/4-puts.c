1b8e0bf4aa1c1e4a6bfe8d06368de9d9c2d65aef
Raymond Oluoch <rasholuoch@gmail.com>
2021-02-03 14:23:06 +0000

    writing program with c in shell command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Prints a string
+ *
+ *Return:always 0 (sUCCESS)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
